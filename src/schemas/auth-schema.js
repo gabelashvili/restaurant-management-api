a5292b3a1fdb338911e81ac6800c94f622db81c5
gabelashvili <gabelashvili1999@gmail.com>
2023-12-10 23:47:25 +0400

    fx

--- src/schemas/auth-schema.js
+++ src/schemas/auth-schema.js
@@ -1,13 +1,13 @@
-import joi from 'joi';
+import * as yup from 'yup';
 
-export const updatePasswordSchema = joi.object({
-  currentPassword: joi.string().required(),
-  newPassword: joi.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,30}$/),
+export const updatePasswordSchema = yup.object({
+  currentPassword: yup.string().required(),
+  newPassword: yup.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,30}$/),
 });
 
-export const updateDetailsSchema = joi.object({
-  firstName: joi.number().min(1),
-  lastName: joi.number().min(1),
+export const updateDetailsSchema = yup.object({
+  firstName: yup.number().min(1),
+  lastName: yup.number().min(1),
   // eslint-disable-next-line max-len
-  email: joi.string().pattern(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+  email: yup.string().pattern(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
 }).min(1);
