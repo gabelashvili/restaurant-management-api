c9a3108ece4e015fc966ff20039dfc11ac88d4fd
gabelashvili <gabelashvili1999@gmail.com>
2023-12-10 23:50:27 +0400

    refactor

--- src/schemas/auth-schema.js
+++ src/schemas/auth-schema.js
@@ -2,12 +2,12 @@ import * as yup from 'yup';
 
 export const updatePasswordSchema = yup.object({
   currentPassword: yup.string().required(),
-  newPassword: yup.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,30}$/),
+  newPassword: yup.string().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,30}$/),
 });
 
 export const updateDetailsSchema = yup.object({
   firstName: yup.number().min(1),
   lastName: yup.number().min(1),
   // eslint-disable-next-line max-len
-  email: yup.string().pattern(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
-}).min(1);
+  email: yup.string().matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+}).atLeastOneOf(['firstName', 'lastName', 'email']);
